812b274dd2a5862c635a4288340bbec252684ba4
Fernanda Meheust <fernanda.meheust@oracle.com>
2023-07-06 22:25:46 +0200

    Application information + output for LB IP + deploy script (#7)
    * Application information + output for LB IP + deploy script
    
    * Version

--- output.tf
+++ output.tf
@@ -5,5 +5,5 @@
 
 output "app_url" {
   description = "Application URL : "
-  value = (var.create_fqdn ? "https://${local.domain_name}" : oci_load_balancer.flexible_loadbalancer.ip_address_details[0].ip_address)
+  value = (var.create_fqdn ? "https://${local.domain_name}" : "http://${oci_load_balancer.flexible_loadbalancer.ip_address_details[0].ip_address}")
 }
