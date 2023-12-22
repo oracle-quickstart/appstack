d4ac00406aafb2ca1664a57f5c3feee7dca9eb90
Fernanda Meheust <fernanda.meheust@oracle.com>
2023-12-22 18:41:42 +0100

    source type

--- devops.tf
+++ devops.tf
@@ -181,12 +181,12 @@ resource "oci_devops_build_pipeline_stage" "art_build_pipeline_stage" {
 resource "oci_devops_deploy_artifact" "container_image_artifact" {
   argument_substitution_mode = "NONE"
   deploy_artifact_type       = "OCIR"
-  deploy_artifact_source_type = "OCIR"
   project_id                 = local.project_id
   display_name               = "Container image"
 
   deploy_artifact_source {
     image_uri = local.image-latest-tag
+    deploy_artifact_source_type = "OCIR"
   }
 }
 
